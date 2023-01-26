5be92ab67027013dab2824ec57b7a9c2ccb2e1fd
Nicole <nicole.georgievva@gmail.com>
2023-01-26 21:50:24 +0200

    Finalize the game

--- hangman/src/main/kotlin/Hangman.kt
+++ hangman/src/main/kotlin/Hangman.kt
@@ -1,5 +1,3 @@
-import kotlin.random.Random
-
 val words = listOf(
     "apple", "laptop", "store", "style", "drawing", "house", "clock", "tower", "show", "playstation",
     "table", "flower", "lizard", "sofa", "cupboard", "tomato", "pillow", "star", "circle", "jellyfish"
@@ -15,12 +13,67 @@ fun main() {
 }
 
 fun setupGame() {
-    val wordIndex = Random.nextInt(words.size)
-    word = words[wordIndex].toUpperCase()
+    word = words.shuffled().first().toUpperCase()
 
-    println(word)
+    //adding as many "_" as the word is long.
+    for (i in word.indices) {
+        guesses.add('_')
+    }
 
-    println(print0Mistakes())
+    var gameOver = false
+
+    do {
+        println(printGameStatus())
+        println("Please enter a letter:")
+        val input = readln()
+
+        if (input.isEmpty()) {
+            println("That's not a valid input. Please try again.")
+        } else {
+            val letter = input[0].toUpperCase()
+            if (word.contains(letter)) {
+                for (i in word.indices) {
+                    if (word[i] == letter) {
+                        guesses[i] = letter
+                    }
+                }
+
+                //if we have no more '_', then the game is over
+                if (!guesses.contains('_')) gameOver = true
+            } else {
+                println("Sorry, that's not part of the word.")
+                remainingGuesses--
+                mistakes++
+                if (mistakes == 6) gameOver = true
+            }
+        }
+    } while (!gameOver)
+
+    if (mistakes == 6) {
+        printGameStatus()
+        println("Sorry, you lost. The word was: \n$word")
+    } else {
+        println("\nCongratulations, you won!")
+        println("The word was: \n$word")
+    }
+}
+
+fun printGameStatus() {
+    when (mistakes) {
+        0 -> print0Mistakes()
+        1 -> print1Mistake()
+        2 -> print2Mistakes()
+        3 -> print3Mistakes()
+        4 -> print4Mistakes()
+        5 -> print5Mistakes()
+        6 -> print6Mistakes()
+    }
+
+    print("Word: ")
+    for (element in guesses) {
+        print("$element ")
+    }
+    println("\nYou have $remainingGuesses guess/es left.")
 }
 
 fun print0Mistakes() {
@@ -30,6 +83,72 @@ fun print0Mistakes() {
     println("  |        ")
     println("  |        ")
     println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print1Mistake() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print2Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |      | ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print3Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /| ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print4Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print5Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |     /  ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print6Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |     / \\")
     println(" /|\\      ")
     println("/ | \\     ")
 }
